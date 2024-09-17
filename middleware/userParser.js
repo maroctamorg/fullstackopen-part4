2872e667c8ffa9a5b1467333aee606b35f68833a
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2024-09-17 07:08:07 +0300

    remove debug print from userParser

--- middleware/userParser.js
+++ middleware/userParser.js
@@ -1,12 +1,10 @@
 const jwt = require('jsonwebtoken')
 
 const userParser = async (request, response, next) => {
-    console.log('request.token:', request.token)
     const decodedToken = request.token === undefined
         ? undefined
         : jwt.verify(request.token, process.env.SECRET)
 
-    console.log('decodedToken:', decodedToken)
     request.user = decodedToken?.id
     next()
 }
